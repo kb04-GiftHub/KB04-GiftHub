5cf2e52ffc95423b444feb2ccce33301be5ae659
Jun <116653748+SamGentlee@users.noreply.github.com>
2023-09-04 12:54:05 +0900

    Basic Setting
     초기 프로젝트 설정 - 스프링부트파일
    +롬복

---
+++ GiftHub/src/main/java/mulcam/kb04/gifthub/GiftHub/GiftHubApplication.java
@@ -0,0 +1,13 @@
+package mulcam.kb04.gifthub.GiftHub;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GiftHubApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(GiftHubApplication.class, args);
+	}
+
+}
---
+++ GiftHub/src/test/java/mulcam/kb04/gifthub/GiftHub/GiftHubApplicationTests.java
@@ -0,0 +1,13 @@
+package mulcam.kb04.gifthub.GiftHub;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class GiftHubApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
