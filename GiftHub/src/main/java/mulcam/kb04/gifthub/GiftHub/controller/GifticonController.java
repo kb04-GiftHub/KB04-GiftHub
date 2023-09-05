017cb9d03c83237569f2a69a93d388b6515b625d
dolssun <76782981+dolssun@users.noreply.github.com>
2023-09-05 17:44:57 +0900

    chore : fix

--- GiftHub/src/main/java/mulcam/kb04/gifthub/GiftHub/controller/GifticonController.java
+++ GiftHub/src/main/java/mulcam/kb04/gifthub/GiftHub/controller/GifticonController.java
@@ -8,7 +8,7 @@ import org.springframework.web.bind.annotation.GetMapping;
 public class GifticonController {
 	
 	
-	@GetMapping("/project")
+	@GetMapping("/about")
 	public String gifticon_add() {
 		//model.addAttribute("msg", msg);
 		
