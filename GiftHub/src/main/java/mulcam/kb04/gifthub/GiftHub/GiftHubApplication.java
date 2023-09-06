f35055c661bbc005e74374eeab3f1ba78e4d1ae3
YouJin-Roh <89447924+YouJinRoh-SSU@users.noreply.github.com>
2023-09-06 13:12:18 +0900

    homecontroller 생성
    homecontroller 생성

---
+++ GiftHub/src/main/java/mulcam/kb04/gifthub/GiftHub/GiftHubApplication.java
@@ -0,0 +1,15 @@
+package mulcam.kb04.gifthub.GiftHub;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+import lombok.extern.log4j.Log4j2;
+
+@Log4j2
+@SpringBootApplication
+public class GiftHubApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(GiftHubApplication.class, args);
+	}
+}
---
+++ GiftHub/src/main/java/mulcam/kb04/gifthub/GiftHub/controller/HomeController.java
@@ -0,0 +1,39 @@
+package mulcam.kb04.gifthub.GiftHub.controller;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+
+@Controller
+public class HomeController {
+	//	@Autowired
+	//	private PromotionMapper promotionMapper;
+	@GetMapping("/promotion_insert_form")
+	public String promotion_insert_form() {
+		return "promotion_insert_form";//게시글 등록
+	}
+
+//	@GetMapping("/promotion_detail")
+//	public String promotion_detail(
+//			@RequestParam("promotionNo") int promotionNo, Model model
+//			) {
+//		PromotionDto dto = promotionMapper.findByNo(promotionNo);
+//		return "promotion/detail";//게시글 상세보기
+//	}
+
+//	@GetMapping("/promotion_list")
+//	public String promotion_list(Model model) {
+//		
+//		List<PromotionDto> list = promotionMapper.list();
+//		model.addAttribute("promotion_list",list);
+//		return "promotion/list";//게시글 목록
+//	}
+	@GetMapping("/promotion_delete_form")
+	public String promotion_delete_form() {
+		return "promotion_delete_form";//게시글 등록
+	}
+	
+	@GetMapping("/promotion_update_form")
+	public String promotion_update_form() {
+		return "promotion_update_form";//게시글 등록
+}
+}
