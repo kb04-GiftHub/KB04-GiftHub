72d6af398223237360973c2b1b569d0e9b650154
HurDong <gjehdals456@gmail.com>
2023-09-06 10:28:47 +0900

    commit add entity 0906
    엔터티 칼럼 추가

--- GiftHub/src/main/java/mulcam/kb04/gifthub/GiftHub/entity/Donation.java
+++ GiftHub/src/main/java/mulcam/kb04/gifthub/GiftHub/entity/Donation.java
@@ -2,6 +2,13 @@ package mulcam.kb04.gifthub.GiftHub.entity;
 
 import java.util.Date;
 
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.persistence.JoinColumn;
+import javax.persistence.ManyToOne;
+import javax.persistence.SequenceGenerator;
+
 import lombok.AllArgsConstructor;
 import lombok.Data;
 import lombok.NoArgsConstructor;
@@ -10,10 +17,20 @@ import lombok.NoArgsConstructor;
 @NoArgsConstructor
 @AllArgsConstructor
 public class Donation {
-	private int donationNo, donationAmount;
+	@Id
+	@GeneratedValue(strategy = GenerationType.SEQUENCE, generator = "DONATION_SEQ")
+	@SequenceGenerator(sequenceName = "DONATAION_SEQ", allocationSize = 1, name = "DONTAION_SEQ")
+	private int donationNo;
+
+	private int donationAmount;
+
 	private Date donationDate;
-	private String customerId;
-	private int orgNo;
 
+	@ManyToOne
+	@JoinColumn(name = "customerId")
+	private Customer customerId;
 
+	@ManyToOne
+	@JoinColumn(name = "orgNo")
+	private DonationOrg orgNo;
 }
--- GiftHub/src/main/java/mulcam/kb04/gifthub/GiftHub/entity/Gift.java
+++ GiftHub/src/main/java/mulcam/kb04/gifthub/GiftHub/entity/Gift.java
@@ -2,6 +2,13 @@ package mulcam.kb04.gifthub.GiftHub.entity;
 
 import java.util.Date;
 
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.persistence.JoinColumn;
+import javax.persistence.ManyToOne;
+import javax.persistence.SequenceGenerator;
+
 import lombok.AllArgsConstructor;
 import lombok.Data;
 import lombok.NoArgsConstructor;
@@ -10,8 +17,22 @@ import lombok.NoArgsConstructor;
 @NoArgsConstructor
 @AllArgsConstructor
 public class Gift {
-	private int giftNo, giftStatus, buyNo;
-	private String giftBarcode, customerId;
+	@Id
+	@GeneratedValue(strategy = GenerationType.SEQUENCE, generator = "GIFT_SEQ")
+	@SequenceGenerator(sequenceName = "GIFT_SEQ", allocationSize = 1, name = "GIFT_SEQ")
+	private int giftNo;
+
+	private int giftStatus;
+
+	@JoinColumn(name = "buyNo")
+	private Buy buyNo;
+
+	private String giftBarcode;
+
+	@ManyToOne
+	@JoinColumn(name = "customerId")
+	private Customer customerId;
+
 	private Date giftExp;
 
 }
