1c95661770afac6316fe1c6d9636df26f7da1e37
ISMINIMIN <summmm.224@gmail.com>
2023-09-06 16:06:58 +0900

    mypage 수정
    StoreRepository 수정
    mypage jsp, controller 수정
    application.properties 속성 추가

--- GiftHub/src/main/java/mulcam/kb04/gifthub/GiftHub/controller/MyPageController.java
+++ GiftHub/src/main/java/mulcam/kb04/gifthub/GiftHub/controller/MyPageController.java
@@ -1,11 +1,22 @@
 package mulcam.kb04.gifthub.GiftHub.controller;
 
+import java.util.List;
+
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+
+import mulcam.kb04.gifthub.GiftHub.entity.Store;
+import mulcam.kb04.gifthub.GiftHub.repository.StoreRepository;
 
 @Controller
 public class MyPageController {
+	
+	@Autowired
+	private StoreRepository repository;
 
 	@GetMapping("/mypage/check_pwd")
 	public String check_pwd() {
@@ -13,7 +24,20 @@ public class MyPageController {
 	}
 	
 	@PostMapping("/mypage/check_action")
-	public String check_action() {
+	public String check_action(@RequestParam String pwd, @RequestParam String pwd_chk, Model model) {
+		Store store = repository.findByStoreId("store");
+		if(pwd.equals(pwd_chk)) {
+			if(store.getStorePwd().equals(pwd)) {
+				model.addAttribute("msg", "본인인증 성공");
+				return "mypage/update_info";
+			}
+		} else {
+			model.addAttribute("msg", "비밀번호 불일치");
+			return "mypage/update_info";
+		}
+		
+		
+		model.addAttribute("msg", "본인인증 실패");
 		return "mypage/update_info";
 	}
 	
