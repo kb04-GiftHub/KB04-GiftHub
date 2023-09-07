75fe76fb7d6380cbc1d6950b52710e6a405ae83b
HurDong <gjehdals456@gmail.com>
2023-09-07 10:28:35 +0900

    commit 0907 01

--- GiftHub/src/main/java/mulcam/kb04/gifthub/GiftHub/service/GiftUsedService.java
+++ GiftHub/src/main/java/mulcam/kb04/gifthub/GiftHub/service/GiftUsedService.java
@@ -1,19 +1,5 @@
 package mulcam.kb04.gifthub.GiftHub.service;
 
-import java.util.Date;
-
-import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.stereotype.Service;
-
-import mulcam.kb04.gifthub.GiftHub.repository.GiftUsedRepository;
-
-@Service
-public class GiftUsedService {
-
-	@Autowired
-	private GiftUsedRepository giftUsedRepository;
-
-	public long getCountBetweenDates(Date startDate, Date endDate) {
-		return giftUsedRepository.countByUsedDateBetween(startDate, endDate);
-	}
+public interface GiftUsedService {
+	Long getCountByMonth(int month);
 }
