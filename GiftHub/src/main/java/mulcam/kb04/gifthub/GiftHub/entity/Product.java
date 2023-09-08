ec6910f7eba331feb90e09c7f62ac851fe4ad58c
dolssun <76782981+dolssun@users.noreply.github.com>
2023-09-08 09:21:48 +0900

    fix : ServiceImpl
    - ProductServiceImpl 구현
    - gifticon>add_form => product>add_form  으로 변경

--- GiftHub/src/main/java/mulcam/kb04/gifthub/GiftHub/entity/Product.java
+++ GiftHub/src/main/java/mulcam/kb04/gifthub/GiftHub/entity/Product.java
@@ -11,13 +11,16 @@ import javax.persistence.ManyToOne;
 import javax.persistence.SequenceGenerator;
 
 import lombok.AllArgsConstructor;
+import lombok.Builder;
 import lombok.Data;
 import lombok.NoArgsConstructor;
+import mulcam.kb04.gifthub.GiftHub.dto.ProductDto;
 
 @Entity
 @Data
 @NoArgsConstructor
 @AllArgsConstructor
+@Builder
 public class Product {
 	
 	@Id
@@ -27,10 +30,28 @@ public class Product {
 	
 	private int productPrice;
 	private String productName, productMemo, productImage;
-	private int productExp;
+	private Date productExp;
 
 	@ManyToOne
 	@JoinColumn(name = "storeId")
 	private Store storeId;
 
+	public static Product dtoToEntity(ProductDto dto) {
+		Store store = new Store();
+		//Date date = new Date();
+		
+		store.setStoreId(dto.getStoreId());
+		//date.setDate(dto.getProductExp());
+		
+		return Product.builder()
+				.productNo(dto.getProductNo())
+				.productPrice(dto.getProductPrice())
+				.productName(dto.getProductName())
+				.productMemo(dto.getProductMemo())
+				.productImage(dto.getProductImage())
+				.storeId(store)
+				.productExp(dto.getProductExp())
+				.build();
+	}
+	
 }
