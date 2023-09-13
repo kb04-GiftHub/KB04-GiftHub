f3fdb300f6279ec918756d14f532943a65412fb5
ISMINIMIN <summmm.224@gmail.com>
2023-09-13 17:03:40 +0900

    fix: update mypage use_detail
    - 보유내역 상세 수정
    - 찜 내역 -ing

--- GiftHub/src/main/java/mulcam/kb04/gifthub/GiftHub/dto/JjimDto.java
+++ GiftHub/src/main/java/mulcam/kb04/gifthub/GiftHub/dto/JjimDto.java
@@ -1,15 +1,24 @@
 package mulcam.kb04.gifthub.GiftHub.dto;
 
 import lombok.AllArgsConstructor;
+import lombok.Builder;
 import lombok.Data;
 import lombok.NoArgsConstructor;
+import mulcam.kb04.gifthub.GiftHub.entity.Jjim;
 
 @Data
 @NoArgsConstructor
 @AllArgsConstructor
+@Builder
 public class JjimDto {
 	private int jjimNo, productNo;
 	private String customerId;
 	
-
+	public static JjimDto entityToDto(Jjim entity) {
+		return JjimDto.builder()
+				.jjimNo(entity.getJjimNo())
+				.productNo(entity.getProductNo().getProductNo())
+				.customerId(entity.getCustomerId().getCustomerId())
+				.build();
+	}
 }
